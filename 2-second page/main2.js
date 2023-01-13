9f965c27640cda095d22f51e779fe6118259e991
kanibadavi <110975367+kanibadavi@users.noreply.github.com>
2023-01-13 11:19:18 +0330

    new changes

---
+++ 2-second page/main2.js
@@ -0,0 +1,47 @@
+const key = "S5VeJeskwaa_zsQ8kCfXMJ_ss0x_BXhlj-z99tkA40E";
+
+const links = [
+  "15 Foods That Help With Bloating",
+  "The One Breakfast Brain Experts Want You to Eat More Often",
+  "15 Surprising Foods With Added Sugar",
+  "How Bad Is It Really to Eat Farm-Raised Salmon?",
+  "How to Make a Mug Omelet for a Quick High-Protein Breakfast",
+  "15 Ways to Get More Protein at Breakfast, Lunch and Dinner",
+  "What Really Happens to Your Body When You Drink Bulletproof Coffee",
+  "15 High-Protein Vegetarian Soups That Serve Comfort",
+  "Want to Age Well? Add This Anti-Inflammatory Spice to Your Daily Diet"
+]
+
+let url = `https://api.unsplash.com/search/photos?query=diet&client_id=${key}&orientation=landscape&page=3&per_page=9`;
+
+async function data(url) {
+  try {
+    const response = await fetch(url);
+    const users = await response.json();
+    console.log(users);
+    makeImages(users.results);
+  } catch (err) {
+    console.log(err);
+  }
+}
+data(url);
+
+function makeImages(data2) {
+  console.log(data2);
+  const main = document.querySelector("main");
+  const container = document.createElement("article");
+  container.classList.add("container2");
+  main.append(container);
+  data2.forEach((users, index) => {
+    const image3 = document.createElement("img");
+    image3.src = users.urls.small;
+    const link = document.createElement("a");
+    const div = document.createElement("div");
+    div.classList.add("div");
+    link.href = "#";
+    link.textContent = links[index];
+    div.append(image3, link);
+    container.append(div);
+    image3.classList.add("image3");
+  });
+}
--- 3-third page/main3.js
+++ 3-third page/main3.js
@@ -1,17 +1,17 @@
 const key = "S5VeJeskwaa_zsQ8kCfXMJ_ss0x_BXhlj-z99tkA40E";
-const links = [
-  "What Is Productivity Guilt, and How Can You Overcome It?",
-  "This 20-Minute Lower-Body Workout Builds Muscle With No Equipment",
-  "How to Do a Burpee for Total-Body Strength — Plus How to Modify It for Any Ability",
-  "Your Stay-Healthy Toolkit",
-  "How Bad Is It Really to Never Clean Your Workout Equipment?",
-  "A 10-Minute Seated Core Workout for People With Back Pain",
-  "our challenges",
-  "20-Minute Workouts",
-  "Can't Do a Chaturanga? Here's What Your Body's Trying to Tell You",
-];
 
-let url = `https://api.unsplash.com/search/photos?query=workout&client_id=${key}&orientation=landscape&page=3&per_page=9`;
+const links =[
+  "The 7 Best Workout Journals to Track Your Progress",
+  "How to Do a Burpee for Total-Body Strength — Plus How to Modify It for Any Ability",
+  "Are You Overtraining or Is It a Mental Health Issue? Answering These 3 Questions Can Help You Tell the Difference",
+  "This 20-Minute Lower-Body Workout Builds Muscle With No Equipment",
+  "The 10-3-45 Dumbbell Workout Builds a Stronger Upper Body",
+  "The Windshield Wiper Exercise Takes Your Ab Workouts to the Next Level",
+  "Want to Age Well? Here's the One Surprising Thing You Should Do Every Day","This Yoga Pose Improves Posture and Relaxes Your Mind",  "The Only 5 Exercises Women Need to Get Lean"
+]
+
+let url = `https://api.unsplash.com/search/photos?query=fitness&client_id=${key}&orientation=landscape&page=4&per_page=9`;
+
 async function data(url) {
   try {
     const response = await fetch(url);
@@ -23,22 +23,6 @@ async function data(url) {
   }
 }
 data(url);
-
-let slideIndex = 0;
-showSlides();
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
 
 function makeImages(data2) {
   console.log(data2);
