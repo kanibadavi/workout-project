a2a5a1bca817c748d0a96f141d9118db1559c643
kani-badavi <kanibadavi666@gmail.com>
2023-01-25 08:34:15 +0330

    change

--- main.js
+++ main.js
@@ -24,21 +24,7 @@ async function data(url) {
 }
 data(url);
 
-let slideIndex = 0;
-showSlides();
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
+
 function makeImages(data2) {
  
   const main = document.querySelector("main");
@@ -60,3 +46,18 @@ function makeImages(data2) {
   });
 }
 
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
